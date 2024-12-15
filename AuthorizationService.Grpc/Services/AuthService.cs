f15ef481cde83a00b9da5381ee5dccca83ccb551
s.viatkin <s.viatkin@loymax.ru>
2024-12-15 18:14:50 +0600

    * grpc + mediatr

---
+++ AuthorizationService.Grpc/Services/AuthService.cs
@@ -0,0 +1,32 @@
+﻿using AuthorizationService.Application.Commands;
+using Grpc.Core;
+using MediatR;
+
+namespace AuthorizationService.Grpc.Services;
+
+public class AuthService(IMediator mediator) : Auth.AuthBase
+{
+    public override async Task<UserLoginTransfer> Login(UserLoginRequest userLoginRequest, ServerCallContext context)
+    {
+        if (string.IsNullOrEmpty(userLoginRequest.Username) || string.IsNullOrEmpty(userLoginRequest.Password))
+        {
+            throw new RpcException(new Status(StatusCode.InvalidArgument, "Username and password are required"));
+        }
+        
+        try
+        {
+            var result = await mediator.Send(
+                new UserLoginCommand { Username = userLoginRequest.Username, Password = userLoginRequest.Password });
+            
+            return new UserLoginTransfer { Token = result.Token };
+        }
+        catch (UnauthorizedAccessException e)
+        {   
+            throw new RpcException(new Status(StatusCode.Unauthenticated, e.Message));
+        }
+        catch (Exception ex)
+        {
+            throw new RpcException(new Status(StatusCode.Internal, $"Произошла ошибка при обработке вашего запроса. {ex.Message}"));
+        }
+    }
+}
--- AuthorizationService/AuthorizationService.Application/ServiceCollectionExtensions.cs
+++ AuthorizationService/AuthorizationService.Application/ServiceCollectionExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace AuthorizationService.Application;
 
@@ -6,6 +7,8 @@ public static class ServiceCollectionExtensions
 {
     public static void AddApplication(this IServiceCollection services)
     {
-        
+        var assembly = typeof(ServiceCollectionExtensions).Assembly;
+
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(assembly));
     }
 }
